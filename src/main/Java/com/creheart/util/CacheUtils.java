026520ae3d03416a4df8aeb672bde9c04882f10e
sunny <2623581340@qq.com>
2017-06-04 18:42:34 +0800

    CacheUtils修改生成CacheManager方法

--- src/main/Java/com/creheart/util/CacheUtils.java
+++ src/main/Java/com/creheart/util/CacheUtils.java
@@ -7,19 +7,29 @@ import org.ehcache.xml.XmlConfiguration;
 import java.net.URL;
 
 /**
+ *
  * Created by sunny on 2017/6/4.
  */
 public class CacheUtils {
+
+    private static CacheManager cacheManager = null;
 
     /**
      * 根据EhCache.xml配置生成CacheManager
      *
      * */
     public static CacheManager getXmlConfigCacheManager() {
-        final URL url = CacheUtils.class.getClass().getResource("/EhCache.xml");
-        XmlConfiguration xmlConfiguration = new XmlConfiguration(url);
-        CacheManager cacheManager = CacheManagerBuilder.newCacheManager(xmlConfiguration);
-        cacheManager.init();
+        if (null == cacheManager) {
+            synchronized (CacheManager.class) {
+                if (null == cacheManager) {
+                    final URL url = CacheUtils.class.getClass().getResource("/EhCache.xml");
+                    XmlConfiguration xmlConfiguration = new XmlConfiguration(url);
+                    cacheManager = CacheManagerBuilder.newCacheManager(xmlConfiguration);
+                    cacheManager.init();
+                }
+            }
+        }
+
         return cacheManager;
     }
 }
